f39b1b6a766c012254131c27899ce265a16c2e62
Scott Barton <divideddarko@Gmail.com>
2019-08-05 21:50:20 +0100

    Smile Completed

---
+++ Smile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Smile
+{
+     class Program
+     {
+          public static int CountSmileys(string[] smileys)
+          {
+               int Count = 0;
+
+               Regex rgx = new Regex("[:;]{1}[-~]?[)D]{1}");
+
+               foreach (string Smile in smileys)
+               {
+                    if (rgx.IsMatch(Smile))
+                    {
+                         Count++;
+                    }
+               }
+
+               return Count;
+          }
+     }
+}
