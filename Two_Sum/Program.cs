d73618fc76f3f8059d9cd349dbf735dd1fa22e8a
Scott Barton <divideddarko@gmail.com>
2020-07-10 15:31:24 +0100

    leetcode challenge

--- Two_Sum/Program.cs
+++ Two_Sum/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Two_Sum
 {
-    class Program
+    public class Solution
     {
-        static void Main(string[] args)
+        public int[] TwoSum(int[] nums, int target)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                x = nums[i];
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+                    y = nums[j];
+
+                    if ((x + y) == target)
+                    {
+                        int[] found = { i, j };
+                        return found;
+                    }
+                }
+            }
+
+            int[] nope = { 0 };
+            return nope;
         }
     }
 }
