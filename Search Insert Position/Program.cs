3ca4dbf613365b0479a1421bf86aa91951dd35fd
Scott Barton <divideddarko@gmail.com>
2020-07-18 12:27:15 +0100

    refactored code - cleaner solution

--- Search Insert Position/Program.cs
+++ Search Insert Position/Program.cs
@@ -8,22 +8,20 @@ namespace Search_Insert_Position
         static void Main(string[] args)
         {
             int[] nums = { 1, 3, 5 };
-            int target = SearchInsert(nums, 4);
+            int target = SearchInsert(nums, 7);
             Console.WriteLine(target);
         }
 
         public static int SearchInsert(int[] nums, int target)
         {
-            int cc = 0;
             for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[i] == target || nums[i] > target)
+                if (nums[i] >= target)
                 {
                     return i;
                 }
-                cc = i;
             }
-            return (cc + 1);
+            return nums.Length;
         }
     }
 }
