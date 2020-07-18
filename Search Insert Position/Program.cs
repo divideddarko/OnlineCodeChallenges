0ff3f8b4e4a3377135303258cb9d93b919b6a7c6
Scott Barton <divideddarko@gmail.com>
2020-07-18 12:17:42 +0100

    Leetcode - Search Insert Position

---
+++ Search Insert Position/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Search_Insert_Position
+{
+    // https://leetcode.com/problems/search-insert-position/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 3, 5 };
+            int target = SearchInsert(nums, 4);
+            Console.WriteLine(target);
+        }
+
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int cc = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target || nums[i] > target)
+                {
+                    return i;
+                }
+                cc = i;
+            }
+            return (cc + 1);
+        }
+    }
+}
